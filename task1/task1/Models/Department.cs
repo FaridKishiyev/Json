ecc97b0b907d587a8aba8f3752f0c756a001fa44
FaridKishiyev <99797424+FaridKishiyev@users.noreply.github.com>
2022-04-05 23:49:14 +0400

    Add files via upload

---
+++ task1/task1/Models/Department.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1.Models
+{
+    internal class Department
+    {
+        private int _id;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        List<Employee> Employees { get; set; }
+        public Department()
+        {
+            _id++;
+            Id = _id;
+            Employees = new List<Employee>();
+        }
+
+        public void AddEmployee(Employee employe)
+        {
+            Employees.Add(employe);
+            StreamWriter fw = null;
+            string json = JsonConvert.SerializeObject(Employees);
+            using (fw = new StreamWriter(@"C:\Users\Ferid\source\repos\task1\task1\JsonFolder\database.json"))
+            {
+                fw.WriteLine(json);
+            }
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            Department department = JsonConvert.DeserializeObject<Department>(@"C:\Users\Ferid\source\repos\task1\task1\JsonFolder\database.json");
+            return department.Employees.Find(x => x.Id == id);
+
+        }
+
+        public List<Employee> RemoveEmployee(int id)
+        {
+            Department department = JsonConvert.DeserializeObject<Department>(@"C:\Users\Ferid\source\repos\task1\task1\JsonFolder\database.json");
+            department.Employees.RemoveAll(x => x.Id == id);
+
+            return Employees;
+        }
+
+
+    }
+}
---
+++ task1/task1/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1.Models
+{
+    internal class Employee
+    {
+        private int _id;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee()
+        {
+            _id++;
+            Id = _id;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id: {Id}\nName: {Name}\nSalary: {Salary}");
+        }
+    }
+}
